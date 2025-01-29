ae4844e2073d5954d2d5f1d30fa1bd2a06f63772
abdulllahseker <abdullah_seker34@hotmail.com>
2025-01-29 23:33:13 +0300

    karanlık mod güncelleme

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/core/routes.dart' show router;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized(); // Flutter widget'larını başlat
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Tema modunun kontrolü için ValueNotifier kullanıyoruz
+  final ValueNotifier<ThemeMode> _themeNotifier =
+      ValueNotifier(ThemeMode.light);
+
+  @override
+  void dispose() {
+    _themeNotifier.dispose(); // State'i temizle
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      routerConfig: router, // go_router yapılandırmamızı kullan
-      debugShowCheckedModeBanner: false, // Debug bandını kaldır
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: _themeNotifier,
+      builder: (context, themeMode, child) {
+        return MaterialApp.router(
+          title: 'Flutter App',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            brightness: Brightness.light,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.black),
+            ),
+          ),
+          darkTheme: ThemeData(
+            primarySwatch: Colors.blue,
+            brightness: Brightness.dark,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.white),
+            ),
+          ),
+          themeMode: themeMode, // Burada tema modunu ayarlıyoruz
+          routerConfig: router,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
